cfdb7350de85770f340b7895bb8391ad24a03c3b
jorgeramirez2mt <91513363+jorgeramirez2mt@users.noreply.github.com>
2022-05-07 18:26:54 -0500

    first commit

---
+++ src/main/java/co/jers/base/backendproyect/BackendproyectApplication.java
@@ -0,0 +1,13 @@
+package co.jers.base.backendproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendproyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendproyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/jers/base/backendproyect/BackendproyectApplicationTests.java
@@ -0,0 +1,13 @@
+package co.jers.base.backendproyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendproyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
